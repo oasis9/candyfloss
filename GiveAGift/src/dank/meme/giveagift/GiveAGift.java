a5317953f8bf3fc04f0ac914fc2a43a906118611
oasis9 <bobbitysharp@gmail.com>
2016-11-06 01:26:23 +1300

    Particle patch
    Fix particle placement math

--- GiveAGift/src/dank/meme/giveagift/GiveAGift.java
+++ GiveAGift/src/dank/meme/giveagift/GiveAGift.java
@@ -11,7 +11,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
 
 import net.minecraft.server.v1_9_R1.EnumParticle;
 import net.minecraft.server.v1_9_R1.PacketPlayOutWorldParticles;
@@ -27,32 +26,38 @@ public class GiveAGift extends JavaPlugin {
 		Player pl = (Player) sender;
 		if (cmd.getName().equalsIgnoreCase("gift")) {
 			Location l = pl.getLocation();
+			l.setPitch(0);
 			l.add(l.getDirection().multiply(2));
 			l.setYaw(l.getYaw() + 180 % 360);
 			ArmorStand as = (ArmorStand) l.getWorld().spawnEntity(l, EntityType.ARMOR_STAND);
 			as.setVisible(false);
+			as.setGravity(false);
 			as.setHelmet(new ItemStack(Material.ENDER_CHEST));
 			new BukkitRunnable() {
-				Location pll = pl.getEyeLocation();
-				Location l = new Location(pll.getWorld(), pll.getX(), pll.getY(), pll.getZ());
+				int i = 0;
 				@Override
 				public void run() {
-					double y = l.getY() + (1 / 360 / 2);
-					if (y >= pll.getBlockY() + 1)
+					Location hlx = l.clone();
+					double x = 4;
+					double xpow = Math.pow(x, 2);
+					hlx.add(0, 1.5 + (1d / (360d / xpow) * i), 0);
+					hlx.setYaw((float) (i * xpow % 360));
+					hlx.add(hlx.getDirection().multiply(0.5));
+					if (i > 360 / xpow) {
+						as.remove();
 						cancel();
-					l.setY(y);
-					l.setYaw(l.getYaw() + (360 / 2));
-					Vector v = l.getDirection();
-					l.add(v.multiply(0.5));
-					Location opp = l.clone();
-					opp.subtract(v);
-					PacketPlayOutWorldParticles helix1 = new PacketPlayOutWorldParticles(EnumParticle.FIREWORKS_SPARK, true, (float) l.getX(), (float) l.getY(), (float) l.getZ(), 0, 0, 0, 0, 1);
+						return;
+					}
+					Location opp = hlx.clone();
+					opp.subtract(hlx.getDirection());
+					PacketPlayOutWorldParticles helix1 = new PacketPlayOutWorldParticles(EnumParticle.FIREWORKS_SPARK, true, (float) hlx.getX(), (float) hlx.getY(), (float) hlx.getZ(), 0, 0, 0, 0, 1);
 					PacketPlayOutWorldParticles helix2 = new PacketPlayOutWorldParticles(EnumParticle.FIREWORKS_SPARK, true, (float) opp.getX(), (float) opp.getY(), (float) opp.getZ(), 0, 0, 0, 0, 1);
 					PlayerConnection pc = ((CraftPlayer) pl).getHandle().playerConnection;
 					pc.sendPacket(helix1);
 					pc.sendPacket(helix2);
+					i++;
 				}
-			}.runTaskTimer(this, 20 / 2, 20 / 10);
+			}.runTaskTimer(this, 10, 1);
 		}
 		return true;
 	}
